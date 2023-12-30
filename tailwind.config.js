3d889f5461d1ad530eb9356a27ce3513c24ce5b0
AdamAbd <adamabd14113@gmail.com>
2023-12-30 20:27:10 +0700

    finish slicing mobile leads

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,31 @@ module.exports = {
       fontFamily: {
         'satoshi': ['Satoshi', 'sans-serif'], // Use a generic font fallback
       },
+      colors: {
+        primary: {
+          DEFAULT: '#FF9500',
+          dark: '#FF7A00',
+          darker: '#FF9E16'
+        },
+        secondary: {
+          DEFAULT: '#00263D',
+        },
+        text: {
+          DEFAULT: '#262626',
+        },
+      },
+      backgroundImage: {
+        'hero': "linear-gradient(90deg, rgba(0, 0, 0, 0.75) 0%, rgba(0, 0, 0, 0.00) 71.84%), linear-gradient(0deg, rgba(0, 0, 0, 0.50) 0%, rgba(0, 0, 0, 0.50) 100%), url('../assets/img/hero-leads.jpeg')",
+      },
+    },
+    container: {
+      padding: {
+        DEFAULT: '0.5rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
     },
   },
   plugins: [],
