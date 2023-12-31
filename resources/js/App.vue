cc8668723204d1f19ceacd769b69de938423bd5c
AdamAbd <adamabd14113@gmail.com>
2023-12-31 10:04:54 +0700

    install vue

---
+++ resources/js/App.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1>Welcome to the Laravel, Vue Tailwindss Demo</h1>
+</template>
+
+<script setup></script>
+
+<style></style>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,11 @@
-import './bootstrap';
+// import './bootstrap';
 
-import '../fonts/Satoshi-Black.ttf';
-import '../fonts/Satoshi-Bold.ttf';
-import '../fonts/Satoshi-Light.ttf';
-import '../fonts/Satoshi-Medium.ttf';
-import '../fonts/Satoshi-Regular.ttf';
+// import '../fonts/Satoshi-Black.ttf';
+// import '../fonts/Satoshi-Bold.ttf';
+// import '../fonts/Satoshi-Light.ttf';
+// import '../fonts/Satoshi-Medium.ttf';
+// import '../fonts/Satoshi-Regular.ttf';
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount("#app")
